8ada93c32829094931a2ee0998ba7ebf68f36969
xfoxcom <xfoxcom@gmail.com>
2022-10-31 18:03:29 +0300

    refactor

---
+++ Code Sharing Platform/task/src/platform/Code.java
@@ -0,0 +1,62 @@
+package platform;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Code {
+    private static final String DATE_FORMATTER= "yyyy-MM-dd HH:mm:ss";
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Id
+    private String id;
+    private String code;
+    private String date;
+    private long time;
+    private int views;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean isSecret;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean isExpired;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private LocalTime timeOfExpire;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean onlyTimeRest;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean onlyViewsRest;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean allRest;
+
+    public void setDate(LocalDateTime time) {
+        this.date = time.format(DateTimeFormatter.ofPattern(DATE_FORMATTER));
+    }
+
+    public void setId() {
+        UUID uuid = UUID.randomUUID();
+        this.id = uuid.toString();
+    }
+
+
+    public boolean isExpired() {
+        if (isSecret) {
+            if (time <= 0) return true;
+        }
+        return false;
+    }
+}
---
+++ Code Sharing Platform/task/src/platform/ControllerOfCode.java
@@ -0,0 +1,139 @@
+package platform;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@Controller
+public class ControllerOfCode {
+    private List<Code> codes = new ArrayList<>();
+    private static final String DATE_FORMATTER= "yyyy-MM-dd HH:mm:ss";
+
+    @Autowired
+    CodeRepository repository;
+    public ControllerOfCode(CodeRepository codeRepository) {
+        this.repository = codeRepository;
+    }
+    @GetMapping(value = "/code/{N}", produces = MediaType.TEXT_HTML_VALUE)
+    public String getCode(HttpServletResponse response, @PathVariable String N, Model model) {
+        response.addHeader("Content-Type", "text/html");
+        if (!repository.existsById(N)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        Code code = repository.findById(N).get();
+
+        if (code.isSecret()) {
+            long timeToExp = code.getTimeOfExpire().toSecondOfDay() - LocalTime.now().withNano(0).toSecondOfDay();
+          /*  if (code.getViews() > 0)
+                code.setViews(code.getViews() - 1);
+            if (code.getTime() > 0)
+                code.setTime(timeToExp);
+            */
+
+            if (code.isAllRest()) {
+                code.setViews(code.getViews() - 1);
+                code.setTime(timeToExp);
+            }
+            if (code.isOnlyViewsRest()) {
+                code.setViews(code.getViews() - 1);
+            }
+            if (code.isOnlyTimeRest()) {
+                code.setTime(timeToExp);
+            }
+
+            repository.save(code);
+
+            if (((code.getViews() < 0 | code.isExpired()) & code.isAllRest()) | (code.isOnlyTimeRest() & code.isExpired()) | (code.isOnlyViewsRest() & code.getViews() < 0)) {
+                repository.deleteById(N);
+            }
+
+        }
+        if (!repository.existsById(N)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        model.addAttribute("code", repository.findById(N).get());
+        if (code.isAllRest()) return "getSnippet";
+        if (code.isOnlyViewsRest()) return "getSnippetWithViewsRestrict";
+        if (code.isOnlyTimeRest()) return "getSnippetWithTimeRestriction";
+        return "getSnippetWithoutRestrict";
+    }
+
+    @GetMapping(value = "/api/code/{N}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Code getField(HttpServletResponse response, @PathVariable String N) {
+        response.addHeader("Content-Type", "application/json");
+        if (repository.existsById(N)) {
+            Code code = repository.findById(N).get();
+            LocalTime time = LocalTime.parse(code.getDate().split("\\s+")[1]); // time of creation
+
+            if (code.isSecret()) {
+                long timeToExp = code.getTimeOfExpire().toSecondOfDay() - LocalTime.now().withNano(0).toSecondOfDay();
+                if (code.getViews() > 0) code.setViews(code.getViews() - 1);
+                if (code.getTime() > 0) code.setTime(timeToExp);
+                repository.save(code);
+                if ((code.getViews() < 1 & code.isExpired() & code.isAllRest()) | (code.isOnlyTimeRest() & code.isExpired()) | (code.isOnlyViewsRest() & code.getViews() < 1)) {
+                    repository.deleteById(N);
+                }
+            }
+            if (repository.existsById(N)) {
+                return repository.findById(N).get();
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/api/code/new")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> postNewCode(@RequestBody Code code) {
+
+        // TODO: 03.07.2022 delete after test
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+
+        code.setId();
+        code.setDate(LocalDateTime.now());
+        code.setExpired(false);
+        if (code.getTime() <= 0 & code.getViews() <=0) {
+            code.setSecret(false);
+        } else {
+            String[] t = code.getDate().split("\\s+");
+            code.setSecret(true);
+            code.setTimeOfExpire(LocalTime.parse(t[1]).plusSeconds(code.getTime()));
+            if (code.getViews() > 0 & code.getTime() > 0) code.setAllRest(true);
+            if (code.getTime() == 0 & code.getViews() > 0) code.setOnlyViewsRest(true);
+            if (code.getViews() == 0 & code.getTime() > 0) code.setOnlyTimeRest(true);
+        }
+        repository.save(code);
+        return ResponseEntity.ok(Map.of("id", code.getId()));
+    }
+
+    @GetMapping("/code/new")
+    public String createCode() {
+        return "create";
+    }
+
+    @GetMapping("/api/code/latest")
+    @ResponseBody
+    public List<Code> getLatest() {
+       return repository.findAll().stream().filter(c -> !c.isSecret()).sorted(Comparator.comparing(Code::getDate).reversed()).limit(10).collect(Collectors.toList());
+    }
+    @GetMapping("/code/latest")
+    public String getLatestHTML (Model model) {
+        model.addAttribute("codes", repository.findAll().stream().filter(c -> !c.isSecret()).sorted(Comparator.comparing(Code::getDate).reversed()).limit(10).collect(Collectors.toList()));
+        return "latest";
+    }
+}
